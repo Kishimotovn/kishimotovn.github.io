24602376756bf86fa4abda6229a88488370ac9fe
Nhung <Nhung@brighter-connections.com>
2019-03-05 23:46:36 +0000

    update app id and base url

--- js/userfeedwebconnect.js
+++ js/userfeedwebconnect.js
@@ -1,6 +1,6 @@
-var APP_ID = '1445370055764391'; //Facebook APP ID
+var APP_ID = '414066029421126'; //Facebook APP ID
 var LIMIT = 250; //Number of records to bring back at any time
-var BASEURL = 'https://alexfrancisross.github.io/FacebookWebDataConnector/' //base url of virtual directory
+var BASEURL = 'https://kishimotovn.github.io/' //base url of virtual directory
 
 window.fbAsyncInit = function() {
     FB.init({
